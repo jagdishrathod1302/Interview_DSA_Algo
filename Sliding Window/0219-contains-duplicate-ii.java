2b5cace16bd22f4a3ca1c6f8f8531d4f0886f89c
Jagdish Rathod <95543950+jagdishrathod1302@users.noreply.github.com>
2025-07-30 15:01:40 +0530

    Add files via upload

---
+++ Sliding Window/0219-contains-duplicate-ii.java
@@ -0,0 +1,40 @@
+//Approach : Sliding Window 
+// T C     : O(n)
+// S C     : O(n) -> for unorder Set
+
+/*
+Step 1 : if Abs(i-j) > k then shrink Window remove i value from set and increse i++
+Step 2 : if j value present in sait means duplicate found in window return retrue 
+Step   : Otherwise add j value in Set() and increse j pointer further check 
+ */
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+       int n = nums.length;
+
+       Set<Integer> set = new HashSet<>();
+
+       int i = 0, j = 0;
+
+       while(j < n){
+
+          if(Math.abs(i-j) > k){ // opposite abs(i-j) <= k
+            //Shrink Window Increse i Pointer
+            set.remove(nums[i]);
+            i++;
+          }
+
+          //check Previous window value contain or not 
+          if(set.contains(nums[j])){
+            return true;
+          }
+ 
+          set.add(nums[j]);
+          j++;
+
+       }
+
+     return false;
+    }
+}
