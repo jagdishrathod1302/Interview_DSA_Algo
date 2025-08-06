71ed3777b946c561069b831e3387f5d8140f3341
Jagdish Rathod <95543950+jagdishrathod1302@users.noreply.github.com>
2025-08-06 16:06:49 +0530

    Time: 3 ms (94.86%), Space: 61.5 MB (70.68%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,41 +1,30 @@
 //Approach - 2 (Simple Observation)
 //T C : O(n) -> visite element only once 
 //S C : O(1)
-
-/*
-Step 1 : Let know more about e.g nums = {1,3,0,0,2,0,0,4} Right we calcualte zero subArray count
-         int this Example we have 6 subArray of Zero subArray Like this 0,0,00,= 3 , 0,0,00 =3 total 6
-
-Step 2 : Now first we take length of Zero like as while(i<n && nums[i] == 0) lenZero++;
-
-Step 3 : we calculate subArray using this formula result = (lenZero)*(lenZero+1)/2;
-
-Step 4 : return result;
- */
-
 class Solution {
     
     public long zeroFilledSubarray(int[] nums) {
 
-      int i = 0;
-      long result = 0;
-      int n = nums.length;
+       int n = nums.length;
 
-      while(i < n){
+       long result = 0;
 
-           long lenZero = 0;
+       long ZeroCount = 0;
+ 
+     for(int i=0; i<n; i++){
 
-            //count length of Zero 
-            while(i < n && nums[i]== 0){
-                  lenZero++;
-                  i++;                
-            }
+           if(nums[i] == 0){
 
-         //count subArray using this formula 
-         result += (lenZero)*(lenZero+1)/2;
-            i++;
-      }
+                ZeroCount++;
+           }else{
+
+                  ZeroCount = 0;
+           }
+
+           result = result+ZeroCount;
+    }
 
      return result;
+   
     }
 }
