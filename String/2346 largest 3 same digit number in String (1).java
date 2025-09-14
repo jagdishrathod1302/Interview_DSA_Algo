6fd8bc2a382511e85da21343b1bc268b609a0d2a
Jagdish Rathod <95543950+jagdishrathod1302@users.noreply.github.com>
2025-09-14 15:48:57 +0530

    Add files via upload

---
+++ String/2346 largest 3 same digit number in String (1).java
@@ -0,0 +1,26 @@
+//Approach :Simple iteration and check
+// T C : O(n)
+// S C : O(1)
+
+/*
+
+ Step 1: first Create  maxChar where store max value 
+ Step 2 : loop start from 2 the check current number equal with previous num[i-1],num[i-2] means 3 number 
+ Step 3 : the return if not get any 3 digit empty string " "
+ Step 3: if present three digit string return 3 size String with 3 value new String(new char[]{maxChar,maxChar,maxChar}) 
+ */
+class Solution {
+    public String largestGoodInteger(String num) {
+
+     char maxChar = ' ';
+
+     for(int i=2; i<num.length(); i++){
+
+          if(num.charAt(i) == num.charAt(i-1) && num.charAt(i) == num.charAt(i-2)){
+             maxChar = (char) Math.max(maxChar,num.charAt(i));
+          }
+     }
+      return maxChar == ' '?"":new String(new char[]{maxChar,maxChar,maxChar});
+        
+    }
+}
